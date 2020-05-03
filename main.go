d49a8a83f9637af8d412000996bf87de0d4cb02e
Chen33D17017 <s894117@gmail.com>
2020-05-03 16:06:05 +0900

    Add flag fo main.go

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
+//Date ... type for store date
+type Date struct {
+	y int
+	m int
+	d int
+}
+
 func main() {
+	stockNumber := flag.Int("stock", 6501, "The number of stock")
+	start := flag.String("start", "", "Start time in format '20xx.xx'")
+	end := flag.String("end", "", "End time in format '20xx.xx'")
+	flag.Parse()
+
 	f, err := os.OpenFile("data.log", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -21,29 +36,82 @@ func main() {
 		}
 	}()
 
-	addr := "https://stocks.finance.yahoo.co.jp/stocks/history/?code=6501.T"
+	s, err := splitDate(start)
+	if err != nil {
+		log.Fatalf("Fail to extract start data %s\n", err)
+	}
+	e, err := splitDate(end)
+	if err != nil {
+		log.Fatalf("Fail to extract end date %s\n", err)
+	}
+
+	tmp := "https://info.finance.yahoo.co.jp/history/?code=%d.T&sy=%d&sm=%d&sd=%d&ey=%d&em=%d&ed=%d&tm=d"
+
+	addr := fmt.Sprintf(tmp,
+		*stockNumber,
+		s.y, s.m, s.d,
+		e.y, e.m, e.d,
+	)
+
 	resp, err := http.Get(addr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch : %v\n", err)
-		os.Exit(1)
+		log.Fatalf("main : %v\n", err)
 	}
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch : %v\n", err)
-		os.Exit(1)
+		log.Fatalf("main : %v\n", err)
 	}
 
-	TargetProperty := []Propety{
+	defer resp.Body.Close()
+
+	pages := make(chan int, 5)
+	go parsePage(doc, pages)
+	for page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func splitDate(date *string) (Date, error) {
+	var rst Date
+	var err error
+	dateSlice := strings.Split(*date, ".")
+
+	if len(dateSlice) != 3 {
+		return rst, fmt.Errorf("Invalid Format in Date")
+	}
+
+	rst.y, err = strconv.Atoi(dateSlice[0])
+	if err != nil {
+		return rst, fmt.Errorf("Invalid Format on Year")
+	}
+
+	rst.m, err = strconv.Atoi(dateSlice[1])
+	if err != nil {
+		return rst, fmt.Errorf("Invalid Format on Month")
+	}
+
+	rst.d, err = strconv.Atoi(dateSlice[2])
+	if err != nil {
+		return rst, fmt.Errorf("Invalid Format on Month")
+	}
+
+	return rst, nil
+}
+
+func parsePrice(doc *html.Node) {
+
+	// Parse Path for price
+	PriceProperty := []Propety{
 		{"class", "padT12"},
 		{"data", "table"},
 		{"data", "tbody"},
 		{"data", "tr"},
 	}
 
-	nodes, err := (*DomNode)(doc).Select(TargetProperty)
+	nodes, err := (*DomNode)(doc).Select(PriceProperty)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "find Target err: %v\n", err)
+		fmt.Fprintf(os.Stderr, "parsePrice: find Target err: %v", err)
 	}
 	stockData := make([]stock, 0)
 	for _, node := range nodes {
@@ -61,15 +129,31 @@ func main() {
 					parseStockVal(datas[6].Content())})
 		}
 	}
+
 	for n, stock := range stockData {
 		fmt.Printf("%d\t%s\n", n, &stock)
 	}
 }
 
-func tmp() {
-	input := "3,089"
-	val := parseStockVal(input)
-	fmt.Println(val)
-	input2 := "2020年4月21日"
-	fmt.Println(parseDate(input2))
+func parsePage(doc *html.Node, ch chan int) {
+	PageProperty := []Propety{
+		{"class", "ymuiPagingBottom"},
+	}
+
+	nodes, err := (*DomNode)(doc).Select(PageProperty)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "parsePage: find Target err: %v", err)
+	}
+
+	for _, node := range nodes {
+		datas := node.SelectAll(Propety{"data", "a"})
+		for _, data := range datas {
+			content := data.Content()
+			page, err := strconv.Atoi(content)
+			if err == nil {
+				ch <- page
+			}
+		}
+	}
+	close(ch)
 }
--- stock.go
+++ stock.go
@@ -2,30 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"time"
-	"strings"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type stock struct {
-	name string
-	date time.Time
-	open float64
-	high float64
-	low float64
-	end float64
+	name   string
+	date   time.Time
+	open   float64
+	high   float64
+	low    float64
+	end    float64
 	volumn float64
-	close float64
+	close  float64
 }
-
 
 func (s *stock) String() string {
 	return fmt.Sprintf("%s @%s: %f", s.name, s.date, s.close)
-
 }
-
 
 func parseStockVal(s string) float64 {
 	s = strings.ReplaceAll(s, ",", "")
@@ -36,16 +33,14 @@ func parseStockVal(s string) float64 {
 	return rst
 }
 
-
 func parseDate(s string) time.Time {
-		re := regexp.MustCompile(`\d+`)
-		reRst := re.FindAllString(s, -1)
-		year, yearErr := strconv.Atoi(reRst[0])
-		month, monthErr := strconv.Atoi(reRst[1])
-		day, dayErr := strconv.Atoi(reRst[2])
-		if dayErr != nil || monthErr != nil || yearErr != nil{
-			panic("parseDate: fail to convert date")
-		}
-		return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
-		
+	re := regexp.MustCompile(`\d+`)
+	reRst := re.FindAllString(s, -1)
+	year, yearErr := strconv.Atoi(reRst[0])
+	month, monthErr := strconv.Atoi(reRst[1])
+	day, dayErr := strconv.Atoi(reRst[2])
+	if dayErr != nil || monthErr != nil || yearErr != nil {
+		panic("parseDate: fail to convert date")
+	}
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
 }
